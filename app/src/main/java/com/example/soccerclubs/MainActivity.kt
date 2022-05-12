35e71cbb2cd7b457183a7e0f8030eaa6ea0bc5e0
AntonEsipow <esipow.anton99@gmail.com>
2022-05-12 16:13:54 +0300

    Filled the ArrayList<SoccerTile>

--- app/src/main/java/com/example/soccerclubs/MainActivity.kt
+++ app/src/main/java/com/example/soccerclubs/MainActivity.kt
@@ -2,10 +2,78 @@ package com.example.soccerclubs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.soccerclubs.data.SoccerTile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val soccerTileList = getSoccerTileList()
+    }
+
+    private fun getSoccerTileList(): ArrayList<SoccerTile> {
+        return ArrayList<SoccerTile>().apply {
+            add(
+                SoccerTile(
+                    id = "everton",
+                    title = "Everton",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.everton_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/151089497233"
+                ))
+            add(
+                SoccerTile(
+                    id = "westham",
+                    title = "Weatham",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.westham_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/151053901701"
+                ))
+            add(
+                SoccerTile(
+                    id = "paris",
+                    title = "Paris",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.paris_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/151043279087"
+                ))
+            add(
+                SoccerTile(
+                    id = "arsenal",
+                    title = "Arsenal",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.arsenal_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/150401426840"
+                ))
+            add(
+                SoccerTile(
+                    id = "borussia",
+                    title = "Borussia",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.borussia_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/150356303395"
+                ))
+            add(
+                SoccerTile(
+                    id = "juventus",
+                    title = "Juventus",
+                    description = "Description of the club",
+                    descriptionLong = "A longer description of the club that wouldn't fit on a single line",
+                    buttonText = "Learn More",
+                    headerImageResId = R.drawable.juventus_header,
+                    headerImageUrl = "https://f-edits.tumblr.com/image/150299524153"
+                ))
+        }
     }
 }
---
+++ app/src/main/java/com/example/soccerclubs/data/SoccerTile.kt
@@ -0,0 +1,18 @@
+package com.example.soccerclubs.data
+
+data class SoccerTile(
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val descriptionLong: String = "",
+    val buttonText: String = "",
+    val headerImageResId: Int = 0,
+    val headerImageUrl: String? = null,
+)
+
+// everton https://f-edits.tumblr.com/image/151089497233
+// westham https://f-edits.tumblr.com/image/151053901701
+// paris https://f-edits.tumblr.com/image/151043279087
+// arsenal https://f-edits.tumblr.com/image/150401426840
+// borussia https://f-edits.tumblr.com/image/150356303395
+// juventus https://f-edits.tumblr.com/image/150299524153
